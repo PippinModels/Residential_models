a06482c7f22c8cbe3a79be238b99881cc7deb27b
PippinModels <ppandit@pippintitle.com>
2025-06-25 07:32:48 -0500

    Update residential_pricing_model_streamlit.py

--- residential_pricing_model_streamlit.py
+++ residential_pricing_model_streamlit.py
@@ -90,7 +90,7 @@ if not df.empty:
                 range_key = (lo, hi)
                 if range_key not in seen_ranges:
                     seen_ranges.add(range_key)
-                    option_text = f"${lo:,}" if lo == hi else f"${lo:,} - ${hi:,}"
+                    option_text =  f"${lo:,}" if lo == hi else f"${lo:,} – ${hi:,}"
                     formatted_options[option_text] = (label, desc, lo, hi)
 
             st.session_state.prediction_choices = formatted_options
